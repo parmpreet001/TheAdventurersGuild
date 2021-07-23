608891d4ae6cf828e6e3a0778158ea7cde3419c3
parmpreet001 <parmpreet1337@gmail.com>
2021-07-23 12:41:01 -0700

    Added GoalType and TargetType enums

---
+++ Assets/Scripts/Enums/GoalType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GoalType determines what a faction wants to do in regards to a certain attribute. 
+/// LOWER: Take actions that will lower the attribute
+/// RAISE: Take actions that will raise the attribute
+/// </summary>
+public enum GoalType
+{
+    LOWER, RAISE
+}
---
+++ Assets/Scripts/Enums/TargetType.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// TargetType determines whether a faction's goals or actions are targeted towards their own faction, other factions, or both
+/// </summary>
+public enum TargetType
+{
+    SELF, OTHER, BOTH
+}
