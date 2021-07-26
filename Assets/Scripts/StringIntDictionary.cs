13d0425720d25634fb9afa5f7834001a8dbbd5ed
parmpreet001 <parmpreet1337@gmail.com>
2021-07-26 13:02:24 -0700

    Updated StringIntDictionary.cs
    Added minValue and maxValue parameters.

--- Assets/Scripts/StringIntDictionary.cs
+++ Assets/Scripts/StringIntDictionary.cs
@@ -15,11 +15,23 @@ public class StringIntDictionary
     {
         public string name;
         public int amount;
+        public int minValue;
+        public int maxValue;
 
         public Item(string name, int amount)
         {
             this.name = name;
             this.amount = amount;
+            minValue = 0;
+            maxValue = 100;
+        }
+
+        public Item(string name, int amount, int minValue, int maxValue)
+        {
+            this.name = name;
+            this.amount = amount;
+            this.minValue = minValue;
+            this.maxValue = maxValue;
         }
     }
 
@@ -55,6 +67,36 @@ public class StringIntDictionary
         if (!itemAlreadyExists)
         {
             items.Add(new Item(tempName, amount));
+        }
+    }
+
+    /// <summary>
+    /// Will attempt to add an item to the list of items. If an item already exists, the function will
+    /// simply modify the value.
+    /// </summary>
+    /// <param name="name">Name of the item to be added</param>
+    /// <param name="amount">Amount of the item to add/subtract</param>
+    /// <param name="minValue">Lowest value that can be set</param>
+    /// <param name="maxValue">Highest value that can be set</param>
+    public void AddItem(string name, int amount, int minValue, int maxValue)
+    {
+        bool itemAlreadyExists = false;
+
+        string tempName = name.Trim();
+        tempName = tempName.ToLower();
+
+        foreach (Item item in items)
+        {
+            if (item.name.Equals(tempName))
+            {
+                itemAlreadyExists = true;
+                item.amount += amount;
+            }
+        }
+
+        if (!itemAlreadyExists)
+        {
+            items.Add(new Item(tempName, amount, minValue, maxValue));
         }
     }
 }
