17020f22d2a3b750d3105032f71684d3f274e8d2
parmpreet001 <parmpreet1337@gmail.com>
2021-08-05 22:02:44 -0700

    Updated FactionData.cs

--- Assets/Scripts/ScriptableObjects/FactionData.cs
+++ Assets/Scripts/ScriptableObjects/FactionData.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class FactionData : ScriptableObject
 {
     public string factionName;
-    public StringIntDictionary resources;
-    public StringIntDictionary attributes;
+    public StringIntDictionary resources = new StringIntDictionary(true, false, globalMin: 0);
+    public StringIntDictionary attributes = new StringIntDictionary(true, true, globalMin: 0, globalMax: 100);
+    public StringIntDictionary attributeOpinions = new StringIntDictionary(true, true, globalMin: -1, globalMax: 10);
     public List<Goal> goals;
     public List<Action> actions;
+    public List<FactionData> factionAwareness;
 }
