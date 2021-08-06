d8c47bf95f7daa21d083c25456092cbee1bbe7f0
parmpreet001 <parmpreet1337@gmail.com>
2021-08-06 13:40:35 -0700

    Deleted Faction.cs. Added FactionSystem.cs. Added Paladins faction.

---
+++ Assets/Scripts/FactionSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactionSystem : MonoBehaviour
+{
+    [SerializeField]
+    public List<FactionData> factions;
+
+    private void Awake()
+    {
+        for(int i = 0; i < factions.Count; i++)
+        {
+            factions[i] = ScriptableObject.Instantiate(factions[i]);
+        }
+
+        foreach(FactionData faction in factions)
+        {
+            foreach(FactionData otherFaction in factions)
+            {
+                if(!faction.factionName.Equals(otherFaction.factionName))
+                {
+                    FactionData temp = new FactionData();
+                    temp.factionName = otherFaction.factionName;
+                    faction.factionAwareness.Add(temp);
+
+                    Debug.Log("Added " + temp.factionName + " to " + faction.factionName);
+                }
+            }
+        }
+
+        foreach(FactionData faction in factions)
+        {
+            Debug.Log(faction.factionAwareness.Count);
+        }
+    }
+}
---
+++ Assets/Scripts/ParmsTestScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParmsTestScript : MonoBehaviour
+{
+    private void Start()
+    {
+        /*
+        StringIntDictionary testDictionary;
+        testDictionary.SetGlobalMin(0);
+        testDictionary.SetGlobalMax(10);
+        testDictionary.Add("heresy", 8);
+        testDictionary.Add("deezNuts", 12);
+        testDictionary.Add("neg", -5);
+
+        Debug.Log("Value of heresy: " + testDictionary.GetValue("heresy"));
+        Debug.Log("Value of deezNuts: " + testDictionary.GetValue("deezNuts"));
+        Debug.Log("Value of neg: " + testDictionary.GetValue("neg"));
+        Debug.Log("Value of girlfriend: " + testDictionary.GetValue("girlfriend"));
+
+        testDictionary.Add("aaaaa", 13);
+        */
+    }
+}
