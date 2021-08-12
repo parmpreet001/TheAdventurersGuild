49312f5eb134b15261fdef5482d1940450bcaf85
parmpreet001 <parmpreet1337@gmail.com>
2021-08-11 22:13:49 -0700

    Created Location.cs

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location
+{
+    private const int minDangerLevel = 1;
+    private const int maxDangerLevel = 10;
+
+    public string locationName;
+    public int dangerLevel;
+
+    public int DangerLevel
+    {
+        get { return dangerLevel; }
+        set
+        {
+            if (value < minDangerLevel)
+                dangerLevel = minDangerLevel;
+            else if (value > maxDangerLevel)
+                dangerLevel = maxDangerLevel;
+            else
+                dangerLevel = value;
+        }
+    }
+
+    public List<Location> connectedLocations = new List<Location>();
+
+    public Location(string locationName, int dangerLevel)
+    {
+        this.locationName = locationName;
+        this.dangerLevel = dangerLevel;
+    }
+}
